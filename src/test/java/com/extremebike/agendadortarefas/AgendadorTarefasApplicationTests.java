ce03aba5c83fd5861daf3bc4c1dd6dba5938e81a
Welington <wellingtonfabi@gmail.com>
2025-07-14 21:04:21 -0300

    commit inicial com esqueleto

---
+++ src/test/java/com/extremebike/agendadortarefas/AgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.extremebike.agendadortarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
